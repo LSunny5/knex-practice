37d791f11733f41fc1407dfc9f22d0dd936dd76d
Sunny Lee <leesunny507@gmail.com>
2020-04-09 01:49:59 -0400

    added drills section and shopping list

---
+++ sql-script/create.shopping-list.sql
@@ -0,0 +1,16 @@
+DROP TYPE IF EXISTS grocery;
+CREATE TYPE grocery AS ENUM (
+    'Main',
+    'Snack',
+    'Lunch',
+    'Breakfast'
+);
+
+CREATE TABLE IF NOT EXISTS shopping_list (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    price decimal(10, 2) NOT NULL,
+    date_added TIMESTAMP DEFAULT now() NOT NULL,
+    checked BOOLEAN DEFAULT false NOT NULL,
+    category grocery NOT NULL
+);
---
+++ src/drills.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL,
+  })
+
+//get all items that contain text
+
+
+//A function that takes one parameter for searchTerm which will be any string
+//The function will query the shopping_list table using Knex methods and select the rows which have a name that contains the searchTerm using a case insensitive match.
+
+//get all items paginated
+
+
+
+
+//A function that takes one parameter for pageNumber which will be a number
+//The function will query the shopping_list table using Knex methods and select the pageNumber page of rows paginated to 6 items per page.
+
+//get all items added after date
+
+
+
+//A function that takes one parameter for daysAgo which will be a number representing a number of days.
+//This function will query the shopping_list table using Knex methods and select the rows which have a date_added that is greater than the daysAgo.
+
+
+//get the total cost for each category
+
+
+//A function that takes no parameters
+//The function will query the shopping_list table using Knex methods and select the rows grouped by their category and showing the total price for each category.
+
+
+
